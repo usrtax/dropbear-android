8b807be2120ebc7691609913ad1e11e32bfc6f09
usrtax <i@user.tax>
2022-11-16 01:45:44 +0800

    ♨

--- build-dropbear-android.sh
+++ build-dropbear-android.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -ex
 
-set -e
+DIR=$(dirname $(realpath "$0"))
 
-if [ -z ${TOOLCHAIN} ]; then echo "TOOLCHAIN must be set. See README.md for more information."; exit -1; fi
+cd $DIR
+source ./env.sh
+
+if [ -z ${TOOLCHAIN} ]; then
+  echo "TOOLCHAIN must be set. See README.md for more information."
+  exit -1
+fi
 
 # Setup the environment
 export TARGET=../target
@@ -13,7 +20,7 @@ export VERSION=2018.76
 
 # Download the latest version of dropbear SSH
 if [ ! -f ./dropbear-$VERSION.tar.bz2 ]; then
-    wget -O ./dropbear-$VERSION.tar.bz2 https://matt.ucc.asn.au/dropbear/releases/dropbear-$VERSION.tar.bz2
+  wget -O ./dropbear-$VERSION.tar.bz2 https://matt.ucc.asn.au/dropbear/releases/dropbear-$VERSION.tar.bz2
 fi
 
 # Start each build with a fresh source copy
@@ -23,13 +30,12 @@ tar xjf dropbear-$VERSION.tar.bz2
 # Change to dropbear directory
 cd dropbear-$VERSION
 
-### START -- configure without modifications first to generate files 
+### START -- configure without modifications first to generate files
 #########################################################################################################################
 echo "Generating required files..."
 
 HOST=arm-linux-androideabi
-COMPILER=${TOOLCHAIN}/bin/arm-linux-androideabi-gcc
-STRIP=${TOOLCHAIN}/bin/arm-linux-androideabi-strip
+COMPILER=$CC
 SYSROOT=${TOOLCHAIN}/sysroot
 
 export CC="$COMPILER --sysroot=$SYSROOT"
@@ -37,51 +43,51 @@ export CC="$COMPILER --sysroot=$SYSROOT"
 # Android 5.0 Lollipop and greater require PIE. Default to this unless otherwise specified.
 if [ -z $DISABLE_PIE ]; then export CFLAGS="-g -O2 -pie -fPIE"; else echo "Disabling PIE compilation..."; fi
 sleep 5
-# Use the default platform target for pie binaries 
+# Use the default platform target for pie binaries
 unset GOOGLE_PLATFORM
 
 # Apply the new config.guess and config.sub now so they're not patched
 cp ../config.guess ../config.sub .
-    
-./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog > /dev/null 2>&1
+
+echo $CC
+./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog >/dev/null
 
 echo "Done generating files"
 sleep 2
 echo
 echo
 #########################################################################################################################
-### END -- configure without modifications first to generate files 
+### END -- configure without modifications first to generate files
 
 # Begin applying changes to make Android compatible
 # Apply the compatibility patch
-patch -p1 < ../android-compat.patch
+patch -p1 <../android-compat.patch
 cd -
 
-echo "Compiling for ARM"  
+echo "Compiling for ARM"
 
 cd dropbear-$VERSION
-    
+
 ./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog
 
 make PROGRAMS="$PROGRAMS"
 MAKE_SUCCESS=$?
 if [ $MAKE_SUCCESS -eq 0 ]; then
-	clear
-	sleep 1
-  	# Create the output directory
-	mkdir -p $TARGET/arm;
-	for PROGRAM in $PROGRAMS; do
+  clear
+  sleep 1
+  # Create the output directory
+  mkdir -p $TARGET/arm
+  for PROGRAM in $PROGRAMS; do
 
-		if [ ! -f $PROGRAM ]; then
-    		echo "${PROGRAM} not found!"
-		fi
+    if [ ! -f $PROGRAM ]; then
+      echo "${PROGRAM} not found!"
+    fi
 
-		$STRIP "./${PROGRAM}"
-	done
+    $STRIP "./${PROGRAM}"
+  done
 
-	cp $PROGRAMS $TARGET/arm
-	echo "Compilation successful. Output files are located in: ${TARGET}/arm"
+  cp $PROGRAMS $TARGET/arm
+  echo "Compilation successful. Output files are located in: ${TARGET}/arm"
 else
- 	echo "Compilation failed."
+  echo "Compilation failed."
 fi
-
