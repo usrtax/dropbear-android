353abd54a49aeadf5550c1e316b62523b0a795d5
usrtax <i@user.tax>
2022-11-16 01:46:52 +0800

    ♨

---
+++ env.sh
@@ -0,0 +1,19 @@
+export API=30
+export NDK_VERSION=22.1.7171670
+
+export TARGET=aarch64-linux-android
+
+export os=$(uname -s | awk '{ print tolower($0) }')
+export NDK=$HOME/Library/Android/sdk/ndk/$NDK_VERSION
+export ANDROID_NDK_HOME=$NDK
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$os-x86_64
+
+export AR=$TOOLCHAIN/bin/llvm-ar
+export AS=$TOOLCHAIN/bin/$TARGET-as
+export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+export LD=$TOOLCHAIN/bin/$TARGET-ld
+export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
+export STRIP=$TOOLCHAIN/bin/$TARGET-strip
+
+export PATH=$TOOLCHAIN/bin:$PATH
